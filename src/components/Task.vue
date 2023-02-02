fa834af60d311231fd225be5f59a533153bbd168
Felix Anducho <felix.anducho74@gmail.com>
2023-02-01 18:44:36 -0600

    Populate task component with markup and necessary props

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,9 +1,42 @@
 <template>
-  
+  <article>
+    <h1>{{  title  }}</h1>
+    <span>{{ completed }}</span>
+    <span> {{ date }} </span>
+    <p>{{ description }}</p>
+
+    <div>
+        <h3>Comentarios:</h3>
+        {{ comments }}
+    </div>
+
+    <p>{{ tags }}</p>
+  </article>
 </template>
 
 <script>
 export default {
+    props: {
+        title: {
+            type: String,
+        },
+        completed: {
+            type: Number,
+            default: 0
+        },
+        date: {
+            type: String,
+        },
+        comments: {
+            type: String,
+        },
+        description: { 
+            type: String,
+        },
+        tags: {
+            type: String
+        }
+    }
 
 }
 </script>
