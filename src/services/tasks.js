36948dee13fdbb0355cd87ebae8ce1a1e9a24cde
Felix Anducho <felix.anducho74@gmail.com>
2023-02-14 18:40:27 -0600

    Replace to use env vars

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const config = {
-    headers: { Authorization: `Bearer e864a0c9eda63181d7d65bc73e61e3dc6b74ef9b82f7049f1fc7d9fc8f29706025bd271d1ee1822b15d654a84e1a0997b973a46f923cc9977b3fcbb064179ecd` }
+    headers: { Authorization: process.env.API_TOKEN }
 };
 
 function getTasks() {
